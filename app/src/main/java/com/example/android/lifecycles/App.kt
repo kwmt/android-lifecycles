14b315c35c3602189b31439b5d74cb09f79b3e2d
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-09-14 12:35:51 +0900

    Timber初期化

---
+++ app/src/main/java/com/example/android/lifecycles/App.kt
@@ -0,0 +1,13 @@
+package com.example.android.lifecycles
+
+import android.app.Application
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(DebugTree())
+    }
+}
