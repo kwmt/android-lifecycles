c7316029405ca934040da78ae416a0b0062410c1
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-09-17 23:58:57 +0900

    lifecycler observe

--- app/src/main/java/com/example/android/lifecycles/practice/lifecycle/BoundLocationManager.kt
+++ app/src/main/java/com/example/android/lifecycles/practice/lifecycle/BoundLocationManager.kt
@@ -21,8 +21,10 @@ import android.content.Context
 import android.location.LocationListener
 import android.location.LocationManager
 import android.util.Log
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
 
 object BoundLocationManager {
     fun bindLocationListenerIn(
@@ -38,10 +40,15 @@ object BoundLocationManager {
     )//TODO: Add lifecycle observer
         : LifecycleObserver {
 
+        init {
+            lifecycleOwner.lifecycle.addObserver(this)
+        }
+
         private var mLocationManager: LocationManager? = null
 
         //TODO: Call this on resume
         @SuppressLint("MissingPermission")
+        @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
         fun addLocationListener() {
             // Note: Use the Fused Location Provider from Google Play Services instead.
             // https://developers.google.com/android/reference/com/google/android/gms/location/FusedLocationProviderApi
@@ -66,6 +73,7 @@ object BoundLocationManager {
         }
 
         //TODO: Call this on pause
+        @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
         fun removeLocationListener() {
             if (mLocationManager == null) {
                 return
