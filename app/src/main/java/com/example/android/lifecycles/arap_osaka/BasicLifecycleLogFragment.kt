b0e244fb50f2c3e7ed4075e10aefd5ab803f9681
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-09-21 20:00:31 +0900

    fix name

--- app/src/main/java/com/example/android/lifecycles/arap_osaka/BasicLifecycleLogFragment.kt
+++ app/src/main/java/com/example/android/lifecycles/arap_osaka/BasicLifecycleLogFragment.kt
@@ -6,16 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.android.codelabs.lifecycle.R
 import com.example.android.lifecycles.LifeCycleLogFragment
+import timber.log.Timber
 
 /**
  * A placeholder fragment containing a simple view.
  */
-class BasicLifecycleActivityLogFragment : LifeCycleLogFragment() {
+class BasicLifecycleLogFragment : LifeCycleLogFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Timber.tag("savedInstanceState").d("onCreateView: $savedInstanceState")
         return inflater.inflate(R.layout.fragment_basic_lifecycle, container, false)
     }
 }
