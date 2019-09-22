948e6fbd1f2da1b841036b94ef7c77b8c30fd63a
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-09-22 09:08:05 +0900

    不要になったものを削除

--- app/src/main/java/com/example/android/lifecycles/arap_osaka/BasicLifeCycleActivity.kt
+++ app/src/main/java/com/example/android/lifecycles/arap_osaka/BasicLifeCycleActivity.kt
@@ -1,8 +1,6 @@
 package com.example.android.lifecycles.arap_osaka
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.navigation.findNavController
 import com.example.android.codelabs.lifecycle.R
 import com.example.android.lifecycles.LifeCycleLogActivity
@@ -11,51 +9,13 @@ import kotlinx.android.synthetic.main.activity_basic_lifecycle.toolbar
 
 class BasicLifeCycleActivity : LifeCycleLogActivity() {
 
-    private val fragment1: Fragment by lazy {
-        BasicLifeCycleLogFragment()
-    }
-    private val fragment2: Fragment by lazy {
-        BasicLifeCycleLogFragment2()
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_basic_lifecycle)
         setSupportActionBar(toolbar)
         fab.setOnClickListener { view ->
             findNavController(R.id.nav_host_container).navigate(R.id.basicLifeCycleLogFragment2)
-
         }
-    }
-
-    private fun replace(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.container, fragment)
-            .commitNow()
-    }
-
-    private fun detachNavHostFragment(
-        fragmentManager: FragmentManager,
-        navHostFragment: Fragment
-    ) {
-        fragmentManager.beginTransaction()
-            .detach(navHostFragment)
-            .commitNow()
-    }
-
-    private fun attachNavHostFragment(
-        fragmentManager: FragmentManager,
-        navHostFragment: Fragment,
-        isPrimaryNavFragment: Boolean
-    ) {
-        fragmentManager.beginTransaction()
-            .attach(navHostFragment)
-            .apply {
-                if (isPrimaryNavFragment) {
-                    setPrimaryNavigationFragment(navHostFragment)
-                }
-            }
-            .commitNow()
     }
 }
 
